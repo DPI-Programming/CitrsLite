a6d4746a8ca27e411e8ddcf3ee46bc76fe835421
Daniel Greco <daniel.greco@fdacs.gov>
2024-04-02 10:59:29 -0400

    Initial Upload

---
+++ Helpline/CTS.Business/ViewModels/InformationLinkViewModels/InformationLinkIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helpline.ViewModels.InformationLinkViewModels
+{
+    public class InformationLinkIndexViewModel
+    {        
+        /*View Model for the Information Link View */
+        public IEnumerable<InformationLinkIndexViewModel>? InformationLinks { get; set; }
+    }
+}
